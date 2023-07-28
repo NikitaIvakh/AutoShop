0f32a28e100a8b88a87f54af584def4587a375c7
Nikita Ivakh <maffy145rus@mail.ru>
2023-07-28 12:54:58 +0300

    Create a DAL level in a project

---
+++ AutoShop.DAL/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AutoShop.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoShop.DAL
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Car> Cars { get; set; }
+    }
+}
