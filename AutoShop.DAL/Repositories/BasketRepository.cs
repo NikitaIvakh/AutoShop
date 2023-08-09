8f7a255158cd929c1f41603b7a94014bdacdab07
Nikita Ivakh <maffy145rus@mail.ru>
2023-08-09 10:40:31 +0300

    Create new repositories

---
+++ AutoShop.DAL/Repositories/BasketRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoShop.DAL.Interfaces;
+using AutoShop.Domain.Entity;
+
+namespace AutoShop.DAL.Repositories
+{
+    public class BasketRepository : IBaseRepository<Basket>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public BasketRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task CreateAsync(Basket entity)
+        {
+            await _applicationDbContext.AddAsync(entity);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<Basket> GetAllElements()
+        {
+            return _applicationDbContext.Baskets;
+        }
+
+        public async Task<Basket> UpdateAsync(Basket type)
+        {
+            _applicationDbContext.Baskets.Update(type);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return type;
+        }
+
+        public async Task DeleteAsync(Basket entity)
+        {
+            _applicationDbContext.Baskets.Remove(entity);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ AutoShop.DAL/Repositories/OrderRepository.cs
@@ -0,0 +1,40 @@
+﻿using AutoShop.DAL.Interfaces;
+using AutoShop.Domain.Entity;
+
+namespace AutoShop.DAL.Repositories
+{
+    public class OrderRepository : IBaseRepository<Order>
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public OrderRepository(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task CreateAsync(Order entity)
+        {
+            await _applicationDbContext.AddAsync(entity);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        public IQueryable<Order> GetAllElements()
+        {
+            return _applicationDbContext.Orders;
+        }
+
+        public async Task<Order> UpdateAsync(Order type)
+        {
+            _applicationDbContext.Orders.Update(type);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return type;
+        }
+
+        public async Task DeleteAsync(Order entity)
+        {
+            _applicationDbContext.Orders.Remove(entity);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
